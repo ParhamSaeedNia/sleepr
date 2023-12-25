87547fbd37a9a1a8f82389739164c7c2058e94c1
parham <parhammm76@gmail.com>
2023-12-25 19:47:37 +0330

    Reservations CRUDs

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigurableModuleClass } from '@nestjs/common/cache/cache.module-definition';
 import { ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '../config/config.module';
 
 @Module({
@@ -15,4 +15,8 @@ import { ConfigModule } from '../config/config.module';
     }),
   ],
 })
-export class DatabaseModule extends ConfigurableModuleClass {}
+export class DatabaseModule extends ConfigurableModuleClass {
+  static forFeature(models: ModelDefinition[]) {
+    return MongooseModule.forFeature(models);
+  }
+}
